0098a17afe5f1bca1ab958bf2f709f566472c6a2
luk1337 <priv.luk@gmail.com>
2025-12-17 19:28:16 +0700

    gale: Remove vendor/lineage device framework matrix inclusion
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,6 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    vendor/lineage/config/device_framework_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
 # Inherit the proprietary files
