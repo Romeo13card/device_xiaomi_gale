bba41f9a0742ee13b31f872b14d48a4d7e73b5c3
Mashopy <eliasgheeraert@gmail.com>
2026-02-05 12:38:23 +0100

    gale: Switch to AIDL bluetooth service
    Change-Id: I40550f0f2033e35ea609401f38f7646cb79abe14
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- device.mk
+++ device.mk
@@ -74,9 +74,7 @@ $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio-impl:32 \
-    android.hardware.bluetooth@1.1.vendor:32 \
-    libbluetooth_audio_session:32
+    android.hardware.bluetooth-service.mediatek
 
 # Cgroup
 PRODUCT_COPY_FILES += \
