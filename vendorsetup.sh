7861025200a2fc3644ad8227cd894da5aafc4a2a
vannashcroft <vannashcroft@proton.me>
2026-03-19 20:29:16 +0700

    gale: Track hardware stuffs to own orgs
    Signed-off-by: vannashcroft <vannashcroft@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,9 +7,9 @@ git clone https://github.com/xaveroprjkt/vendor_xiaomi_gale -b lineage-23.2 vend
 git clone https://github.com/xaveroprjkt/kernel_xiaomi_gale -b lineage-23.2 kernel/xiaomi/gale
 
 # Another yet stuffs
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+git clone https://github.com/xaveroprjkt/device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/xaveroprjkt/hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/xaveroprjkt/hardware_mediatek.git -b lineage-23.2 hardware/mediatek
 git clone https://github.com/techyminati/android_vendor_mediatek_ims vendor/mediatek/ims/
 
 read -rp "Do you want to clone the signing keys? (y/N): " a
