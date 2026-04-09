daa76f66cb97dc6169383bd55458f3e4e5680d72
Romeo13card <romeo_13card_fan@outlook.com>
2026-04-09 08:48:03 +0300

    gale:init matrixx 12+

--- matrixx_gale.mk
+++ matrixx_gale.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/gale/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Project Matrixx stuff.
+$(call inherit-product, vendor/matrixx/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_gale
+PRODUCT_NAME := matrixx_gale
 PRODUCT_DEVICE := gale
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -25,3 +25,12 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="vnd_gale-user 12 SP1A.210812.016 V816.0.5.0.UGPMIXM release-keys"
     BuildFingerprint=alps/vnd_gale/gale:12/SP1A.210812.016/V816.0.5.0.UGPMIXM:user/release-keys
+
+#Matrixx Stuff
+MATRIXX_MAINTAINER := Romeo13card
+WITH_GMS := false
+TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_ENABLE_BLUR := true
+BYPASS_CHARGE_SUPPORTED := false
+HBM_SUPPORTED := false
+TARGET_BOOT_ANIMATION_RES := 720
