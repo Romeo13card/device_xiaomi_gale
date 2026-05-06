2bb201a278344c867d03717387081cfb95522658
Romeo13card <romeo_13card_fan@outlook.com>
2026-05-06 20:43:07 +0300

    vendorsetup: Track all remotes to 23.2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,20 @@
 
-
 echo "Cloning Stuffs Needed For Gayleer"
 
 # Stuffs Gayleer
-git clone https://github.com/Mayuri-Chan/proprietary_vendor_xiaomi_gale -b sixteen vendor/xiaomi/gale
-git clone https://github.com/Mayuri-Chan/android_kernel_xiaomi_gale -b cip kernel/xiaomi/gale
+git clone https://github.com/xaveroprjkt/vendor_xiaomi_gale -b lineage-23.2 vendor/xiaomi/gale
+git clone https://github.com/xaveroprjkt/kernel_xiaomi_gale -b lineage-23.2 kernel/xiaomi/gale
 
 # Another yet stuffs
-git clone https://github.com/xaveroprjkt/device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
-git clone https://github.com/xaveroprjkt/android_vendor_mediatek_ims vendor/mediatek/ims/
-git clone https://github.com/VannTakashi/vann_keys -b pos-16 vendor/custom-priv/keys
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+git clone https://github.com/techyminati/android_vendor_mediatek_ims vendor/mediatek/ims/
+
+read -rp "Do you want to clone the signing keys? (y/N): " a
+
+if [[ $a =~ ^[yY]$ ]]; then
+    git clone https://github.com/VannTakashi/vann_keys vendor/lineage-priv/keys
+else
+    echo "Skipping signing keys."
+fi
